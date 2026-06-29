bd4507399c13b2af3be632ac4beec6bd5cb1ee1e
MohammedDabbah <mohammeddabbah02@icloud.com>
2026-06-29 03:39:01 +0300

    fixing double click

--- src/shieldcare/Main.java
+++ src/shieldcare/Main.java
@@ -5,27 +5,50 @@ import shieldcare.boundary.ClaimHandlingScreen;
 import shieldcare.boundary.ReimbursementScreen;
 import shieldcare.database.DBConnection;
 
+import javax.swing.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ShieldCare ECB Java Implementation");
-        System.out.println("----------------------------------");
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(output);
 
-        DBConnection.testConnection();
+        System.setOut(printStream);
+        System.setErr(printStream);
 
-        XMLImportScreen xmlScreen = new XMLImportScreen();
-        xmlScreen.displayImportScreen();
-
-        ClaimHandlingScreen claimScreen = new ClaimHandlingScreen();
-        claimScreen.displayClaimScreen();
-
-        ReimbursementScreen reimbursementScreen = new ReimbursementScreen();
-        reimbursementScreen.displayReimbursementScreen();
-
-        System.out.println("\nPress Enter to exit...");
         try {
-            System.in.read();
+            System.out.println("ShieldCare ECB Java Implementation");
+            System.out.println("----------------------------------");
+            System.out.println("Working directory: " + System.getProperty("user.dir"));
+            System.out.println();
+
+            DBConnection.testConnection();
+
+            XMLImportScreen xmlScreen = new XMLImportScreen();
+            xmlScreen.displayImportScreen();
+
+            ClaimHandlingScreen claimScreen = new ClaimHandlingScreen();
+            claimScreen.displayClaimScreen();
+
+            ReimbursementScreen reimbursementScreen = new ReimbursementScreen();
+            reimbursementScreen.displayReimbursementScreen();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        JTextArea textArea = new JTextArea(output.toString());
+        textArea.setEditable(false);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new java.awt.Dimension(800, 500));
+
+        JOptionPane.showMessageDialog(
+                null,
+                scrollPane,
+                "ShieldCare ECB Output",
+                JOptionPane.INFORMATION_MESSAGE
+        );
     }
 }
